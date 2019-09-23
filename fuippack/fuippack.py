1e54fb4505ce8b72f6a90f86dfebca9f54c089ab
m33ra <machado.meera@gmail.com>
2019-09-23 14:51:57 +0200

    Adding new files to project

---
+++ fuippack/fuippack.py
@@ -0,0 +1,14 @@
+#
+
+def motivation(my_name):
+    """
+    Motivate a person by saying they are awesome.
+    
+    Args:
+        my_name (str): person's name
+    Returns:
+        None
+
+    """
+
+    print(f"Hey, {my_name}, you're awesome!:D")
